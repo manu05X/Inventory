af01a0bffb75dbaa65439cacfb0da4c4d56ea008
MANISH KUMAR <36466808+manu05X@users.noreply.github.com>
2021-04-24 19:11:08 +0530

    server Path

--- app.js
+++ app.js
@@ -56,10 +56,10 @@ app.post('/course', async(req,res) =>{
 
 
 // for opening frontend
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "Client", "build")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "Client", "build", "index.html"));
 });
 
 
